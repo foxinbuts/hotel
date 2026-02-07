b41b17333ec0fe4c03da5300b108eccdc0972f58
foxinbandage <akmammedowjuma@gmail.com>
2026-02-07 23:22:00 +0800

    Добавлены новые файлы: шрифты, изображения итд

---
+++ Js/Лесное.js
@@ -0,0 +1,68 @@
+function hoveronblock1() {
+ document.getElementById("block2").classList.add("active");
+ document.getElementById("block3").classList.add("active");
+ document.getElementById("block4").classList.add("active");
+ document.getElementById("block5").classList.add("active");
+}
+
+function hoveroffblock1() {
+    document.getElementById("block2").classList.remove("active");
+    document.getElementById("block3").classList.remove("active");
+    document.getElementById("block4").classList.remove("active");
+    document.getElementById("block5").classList.remove("active");
+}
+
+function hoveronblock2() {
+    document.getElementById("block1").classList.add("active");
+    document.getElementById("block3").classList.add("active");
+    document.getElementById("block4").classList.add("active");
+    document.getElementById("block5").classList.add("active");
+   }
+   
+function hoveroffblock2() {
+       document.getElementById("block1").classList.remove("active");
+       document.getElementById("block3").classList.remove("active");
+       document.getElementById("block4").classList.remove("active");
+       document.getElementById("block5").classList.remove("active");
+   }
+
+   function hoveronblock3() {
+    document.getElementById("block2").classList.add("active");
+    document.getElementById("block1").classList.add("active");
+    document.getElementById("block4").classList.add("active");
+    document.getElementById("block5").classList.add("active");
+   }
+   
+   function hoveroffblock3() {
+       document.getElementById("block2").classList.remove("active");
+       document.getElementById("block1").classList.remove("active");
+       document.getElementById("block4").classList.remove("active");
+       document.getElementById("block5").classList.remove("active");
+   }
+   function hoveronblock4() {
+    document.getElementById("block2").classList.add("active");
+    document.getElementById("block3").classList.add("active");
+    document.getElementById("block1").classList.add("active");
+    document.getElementById("block5").classList.add("active");
+   }
+   
+   function hoveroffblock4() {
+       document.getElementById("block2").classList.remove("active");
+       document.getElementById("block3").classList.remove("active");
+       document.getElementById("block1").classList.remove("active");
+       document.getElementById("block5").classList.remove("active");
+   }
+   function hoveronblock5() {
+    document.getElementById("block2").classList.add("active");
+    document.getElementById("block3").classList.add("active");
+    document.getElementById("block4").classList.add("active");
+    document.getElementById("block1").classList.add("active");
+   }
+   
+   function hoveroffblock5() {
+       document.getElementById("block2").classList.remove("active");
+       document.getElementById("block3").classList.remove("active");
+       document.getElementById("block4").classList.remove("active");
+       document.getElementById("block1").classList.remove("active");
+   }
+
